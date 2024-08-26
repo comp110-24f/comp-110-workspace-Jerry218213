25cf96ec0d93d97f464bf991209ff68719a47ad7
Jerry Yan <jerryyan@ad.unc.edu>
2024-08-25 23:33:01 -0400

    finished exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730821425"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+print
